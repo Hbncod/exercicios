7d65949c63f993db53dac928210bd99dd0890cf1
hbncod <hbncod@gmail.com>
2019-09-20 11:05:15 -0300

    exercicios 20/09/19

---
+++ impar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 1;
+        
+                while (num != 0) {
+
+                Console.WriteLine("escolha um número");
+                num = double.Parse(Console.ReadLine());
+
+                if (num % 2 == 0){
+                    Console.WriteLine("Seu número é PAR");
+                    }
+                else{
+                    Console.WriteLine("Seu número é IMPAR");  
+                    }
+            }
+        }
+    }
+}
---
+++ imparserio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace imparserio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int nume = 1;
+            int nume;
+
+            for (nume = 1; nume <=100; nume += 2){
+                Console.Write(nume + " ");
+            }
+
+            }
+        
+        }
+}
+
