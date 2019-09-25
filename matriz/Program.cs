0d8a6dc5535b1a875add22fd29a275654d4ffab1
hbncod <hbncod@gmail.com>
2019-09-25 12:07:25 -0300

    aprendendo a usar matriz

---
+++ matriz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int par = 0;
+            int impar = 0;
+            int[] n = new int [6];
+
+            for (int indice = 0; indice <= 5;indice ++){
+            
+                Console.WriteLine($"Escolha seu {indice + 1}° Número");
+                n[indice] = int.Parse(Console.ReadLine());
+            }
+            for(int indice = 0; indice <= 5;indice ++)
+            {
+                if (n[indice] % 2 == 0)
+                {
+                    par = par + 1;
+                    
+                }      
+                else{
+                    impar = impar + 1;
+                }
+            }
+                Console.WriteLine($"Você possui {par} números pares");
+                Console.WriteLine($"Você possui {impar} números impares");
+                
+
+        }
+    }
+}
