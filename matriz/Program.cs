baa8766c85817c37ae1bedb0a343470d8aaa958d
hbncod <hbncod@gmail.com>
2019-09-26 11:18:31 -0300

    exercicios matriz

--- matriz/Program.cs
+++ matriz/Program.cs
@@ -6,17 +6,17 @@ namespace matriz
     {
         static void Main(string[] args)
         {
+            int[] n = new int [6];
             int par = 0;
             int impar = 0;
-            int[] n = new int [6];
 
             for (int indice = 0; indice <= 5;indice ++){
             
                 Console.WriteLine($"Escolha seu {indice + 1}° Número");
                 n[indice] = int.Parse(Console.ReadLine());
             }
-            for(int indice = 0; indice <= 5;indice ++)
-            {
+            for(int indice = 0; indice <= 5;indice ++){
+
                 if (n[indice] % 2 == 0)
                 {
                     par = par + 1;
---
+++ temperatura/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperaturas = new double [12];
+            double maior = 0;
+            double menor = 0;
+
+            for(int i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"Qual a temperatura média do {i + 1}° mês ?");
+                temperaturas[i] = double.Parse(Console.ReadLine());
+            }
+
+            maior = temperaturas[0];
+            menor = temperaturas[0];
+
+            foreach (double temp in temperaturas)
+            {
+                if(temp > maior){
+                    maior = temp;
+                }
+                else if(temp < menor)
+                {
+                    menor = temp;
+                }
+            }   
+
+            Console.WriteLine($"a maior temperatura do ano foi {maior}°C; e a menor foi {menor}°C.");
+
+
+        }
+    }
+}
